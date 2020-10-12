6b78f134e5dd5faa53cac7b5173eaedb59e00d40
Jeff Pan <>
2020-10-12 22:54:56 +0800

    初构网络 10122254

--- DQN/DeepQNetwork.py
+++ DQN/DeepQNetwork.py
@@ -2,21 +2,29 @@ import tensorflow as tf
 from tensorflow.keras import layers
 
 
+class PredictDeepQNet(tf.keras.Model):
+    def __init__(self, input_dim, output_dim):
+        super(PredictDeepQNet, self).__init__()
+        self.dense1 = layers.Dense(16, activation=tf.nn.tanh, input_dim=input_dim)
+        self.dense2 = layers.Dense(output_dim, activation=tf.nn.tanh)
 
-class DeepQNet(object):
-    def __init__(self):
-        pass
-
-
-class TimeEncoder(tf.keras.Model):
-    def __init__(self):
-        super(TimeEncoder, self).__init__()
-        self.gauss = layers.GaussianNoise(1)
-        self.dense1 = layers.Dense(32, activation=tf.nn.tanh, input_dim=88)
-        self.dense2 = layers.Dense(88)
-
-    def call(self, inputs, training=False):
-        inputs = self.gauss(inputs) if training else inputs
+    def call(self, inputs):
         x = self.dense1(inputs)
         output = self.dense2(x)
         return output
+
+class TargetDeepQNet(tf.keras.Model):
+    def __init__(self, input_dim, output_dim):
+        super(TargetDeepQNet, self).__init__()
+        self.dense1 = layers.Dense(16, activation=tf.nn.tanh, input_dim=input_dim)
+        self.dense2 = layers.Dense(output_dim, activation=tf.nn.tanh)
+
+    def call(self, inputs):
+        x = self.dense1(inputs)
+        output = self.dense2(x)
+        return output
+
+if __name__ == "__main__":
+    # INPUT_DIM 表示输入状态的维度
+    # OUTPUT_DIM 可选动作的种类数
+    pass
